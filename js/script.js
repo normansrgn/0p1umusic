4c951f086f5f3453a4efa17e5b00ddb7818308c4
normansrgn <normanmagic2004@gmail.com>
2024-04-11 21:33:53 +0300

    zoom

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const zoomable = document.getElementById("zoomable");
+    const img = zoomable.querySelector("img");
+
+    zoomable.addEventListener("mouseover", function () {
+        img.style.transform = "scale(1.1)";
+    });
+
+    zoomable.addEventListener("mouseout", function () {
+        img.style.transform = "scale(1)";
+    });
+});
